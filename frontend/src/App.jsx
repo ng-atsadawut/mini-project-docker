490e09a01b0c9972d3ca51d52b5c7179bc003d27
Mr'Atsadawut <ng.atsadawut@gmail.com>
2024-09-17 21:30:18 +0700

    feat: add project frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import GetProducts from './GetProducts'
+
+function App() {
+  return (
+    <div>
+      <GetProducts />
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/GetProducts.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import './GetProducts.css';
+
+function GetProducts() {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3000/getProducts')
+            .then(response => response.json())
+            .then(data => setProducts(data))
+            .catch(error => console.log(error));
+    }, []);
+
+    return (
+        <div>
+            <div className="card_container">
+
+        
+            {products.map(product => (
+                <div key={product.product_id} className="card">
+                    <img src={product.product_image_url} alt={product.product_name} />
+                    <h3>{product.product_name}</h3>
+                    <p>Price: {product.product_price}</p>
+                    <p>Quantity: {product.product_quantity}</p>
+                </div>
+            ))}
+        </div>
+        </div>
+    );
+}
+
+export default GetProducts;
